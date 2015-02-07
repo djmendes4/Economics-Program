594103e1cde6eeafd60e79162d98c85295c40da4
Dillon Mendes <djmendes4@gmail.com>
2015-02-06 22:23:29 -0800

    Added Adaptive Layout

---
+++ menu.js
@@ -0,0 +1,14 @@
+//$(document).ready(function(){
+//        if (Modernizr.touch) {
+//                        $(".radio-options").bind("click", function(event) {
+//                                if (!($(this).parent('.radio-container').hasClass("active")))   {
+//                                $(this).parent('.radio-container').addClass("active");
+//                                event.stopPropagation();
+//                                }
+//                        });
+//        $(".toggle").bind("click", function(){
+//                $(this).parents('.radio-container').removeClass("active");
+//                return false;
+//                 });
+//        }
+//})
